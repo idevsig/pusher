ea12a455e2ded6c0430b74afe8f6bd0b0198957d
Jetsung Chan <skiy@jetsung.com>
2022-04-18 22:22:12 +0800

    [no ci]update: contact email

--- tests/Channels/MailerTest.php
+++ tests/Channels/MailerTest.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Pusher.
  *
- * (c) Jetsung Chan <skiy@jetsung.com>
+ * (c) Jetsung Chan <jetsungchan@gmail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
